9faf1491eab4fd33fa20cb4bd3d66a6775d405d8
ThyMinx <james.cairns@outlook.com>
2015-11-27 11:39:11 +0000

    James - Game Manager Adjustments
    Adjusted the game manager a little

--- Snake/Assets/Scripts/Game Scripts/GameManager.cs
+++ Snake/Assets/Scripts/Game Scripts/GameManager.cs
@@ -4,10 +4,11 @@ using System.Collections;
 public class GameManager : MonoBehaviour {
 
     public bool paused = false;
+    public GameObject pauseMenu;
 
     void Start()
     {
-
+        pauseMenu.SetActive(false);
     }
 
     void Update()
@@ -16,11 +17,13 @@ public class GameManager : MonoBehaviour {
         {
             if (paused)
             {
+                pauseMenu.SetActive(false);
                 paused = false;
                 Time.timeScale = 1;
             }
             else
             {
+                pauseMenu.SetActive(true);
                 paused = true;
                 Time.timeScale = 0;
             }
