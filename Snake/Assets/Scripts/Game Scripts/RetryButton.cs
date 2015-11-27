be5b44c0ffa75dbb39adb0c3a02c37e19d650833
ThyMinx <james.cairns@outlook.com>
2015-11-27 11:50:36 +0000

    James - Pause Menu Buttons
    Pause menu buttons complete

---
+++ Snake/Assets/Scripts/Game Scripts/RetryButton.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RetryButton : MonoBehaviour {
+
+    public void OnButtonPress()
+    {
+        Application.LoadLevel(Application.loadedLevel);
+    }
+
+}
