7fd9064f3aff03835eeffd902b0461a1cb60f1cd
ThyMinx <james.cairns@outlook.com>
2015-11-27 11:06:10 +0000

    James - Pause Done
    Done the script for pause and pause menu

---
+++ Snake/Assets/Scripts/Game Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameManager : MonoBehaviour {
+
+    public bool paused = false;
+
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("p"))
+        {
+            if (paused)
+            {
+                paused = false;
+                Time.timeScale = 1;
+            }
+            else
+            {
+                paused = true;
+                Time.timeScale = 0;
+            }
+        }
+    }
+
+}
---
+++ Snake/Assets/Scripts/Game Scripts/MenuButton.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuButton : MonoBehaviour {
+
+
+    public void Example()
+    {
+        Application.LoadLevel(Application.loadedLevel - 1);
+    }
+    
+
+}
